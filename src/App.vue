579b1d24f479ee18c71d95e6cd9be79d503eda23
booldook <booldook@gmail.com>
2020-08-19 11:02:11 +0900

    search-bar

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template lang="pug">
-	div#app
+	div#app.container
 		nav-bar
+		search-bar
 </template>
 
 <script>
 import NavBar from '@/components/NavBar.vue'
+import SearchBar from '@/components/SearchBar.vue'
 
 export default {
 	name: 'app',
 	components: {
 		'nav-bar': NavBar,
+		'search-bar': SearchBar,
 	}
 }
 </script>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+	form(v-on:submit.prevent='onSubmit' id='searchForm' name='searchForm')
+		input.form-control(v-model='search' type="text" placeholder="상품 검색어를 입력하세요." autofocus)
+		i.fa.fa-times-circle(v-show='search.length' v-on:click='onReset')
+</template>
+
+<script>
+export default {
+	name: 'search-bar',
+	data() {
+		return {
+			search: ''
+		}
+	},
+	methods: {
+		onSubmit() {
+
+		},
+		onReset() {
+			this.search = '';
+		}
+	}
+}
+</script>
+
+<style lang="less">
+	.pointer {cursor: pointer;}
+	#searchForm {
+		position: relative;
+		i {position: absolute; right: 1rem; top: 0.625rem; font-size: 1.25rem; .pointer();}
+	}
+</style>
